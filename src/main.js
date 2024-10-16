309689ec4402381ac395c8f52e4bd9e5f096207b
akash-sagar-1 <akash.sagar@ranium.in>
2024-10-16 18:11:58 +0530

    added add jobs and delete jobs functionality and vue toast notification

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import Toast from 'vue-toastification'; 
+import 'vue-toastification/dist/index.css';
 import 'primeicons/primeicons.css';
 import router from './router';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(Toast);
 
 app.mount('#app');
