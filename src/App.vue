684b79e30feb8d46f7f6e509e4b1890723ae6a0d
akash-sagar-1 <akash.sagar@ranium.in>
2024-10-16 11:41:13 +0530

    added navbar,hero,homecard and card coomponent

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Navbar from '@/components/NavBar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+</script>
+
+<template>
+<Navbar />
+<Hero />
+<HomeCards />
+</template>
